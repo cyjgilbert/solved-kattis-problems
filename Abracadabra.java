d1eb79567a55aa18bc404e3f174924ec0bab5eaf
ignitrixdaze1 <35133311+ignitrixdaze1@users.noreply.github.com>
2019-01-06 22:49:26 +0800

    Create Abracadabra.java

---
+++ Abracadabra.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Abracadabra{
+
+public static void main(String[] args){
+
+Scanner j = new Scanner (System.in);
+
+int n = j.nextInt();
+
+for(int i = 0; i < n; i++){
+String a = "Abracadabra";
+System.out.println( (i + 1) + " " + a); // so that the system starts printing from 1 
+
+
+
+}
+
+
+
+
+}
+
+}
